a4ff6e2a65f82ffa8e26dd2389349db00e33ea06
Miroslav Bibik <bibikm@gmail.com>
2026-03-25 15:33:39 +0200

    Initial Commit

---
+++ abook-reaper/abook_reaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  abook_reaperApp.swift
+//  abook-reaper
+//
+//  Created by Miroslav Bibik on 25.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct abook_reaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
